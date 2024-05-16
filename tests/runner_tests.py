e4a46d2cb348708e5ca1c1718b605ff9adff6753
TecnomaLaser <adrian.tripp1@gmail.com>
2024-05-16 22:25:57 +0200

    updated how tests work

--- tests/runner_tests.py
+++ tests/runner_tests.py
@@ -26,83 +26,161 @@ from protslurm.utils.plotting import sequence_logo
 #TODO: @Adrian: For Attnpacker, ligandmpnn and AF please write Tutorials as Jupyter Notebooks in 'examples' Folder.
 #TODO: @Adrian: Please write tests for LigandMPNN running +10 sequences with/ and without pose_options (to test non_batch and batch run)
 
+
+def check_runner(name:str, runner, poses_options:dict, runner_options:dict=None, config=None, check:str=None):
+
+    if check == runner or check == None:
+        if runner == None:
+            return "Not set up"
+        logging.info(f"Running test for {name}...")
+        work_dir = f"out_{name}"
+        if os.path.isdir(work_dir): shutil.rmtree(work_dir)
+        if config == None or all([os.path.isfile(conf) or os.path.isdir(conf) for conf in config]):
+            try:
+                logging.info(f"Initializing poses for {name}...")
+                poses = Poses(**poses_options, work_dir=work_dir, storage_format="json")
+            except:
+                return "Loading poses failed!"
+            try:
+                logging.info(f"Running runner {name}...")
+                runner.run(**runner_options, poses=poses, prefix="test")
+                logging.info(f"Runner {name} passed!")
+                return "Passed"
+            except:
+                logging.warning(f"Runner {name} failed!")
+                return "Failed"
+        else:
+            logging.warning(f"Runner {name} config set up incorrectly!")
+            return "Config fail"
+    else:
+        logging.info(f"Runner {name} not checked.")
+        return "Not checked"
+    
+
 def main(args):
 
     '''.'''
+
     js_dict = {
         "slurm_gpu_jobstarter": SbatchArrayJobstarter(max_cores=10, gpus=1, options="-c1"),
         "local_jobstarter": LocalJobStarter()
     }
-
-    runner_dict = {
-        "ESMFold": "not tested",
-        "AttnPacker": "not tested",
-        "TMalign": "not tested",
-        "TMscore": "not tested",
-    }
-
 
     if args.jobstarter:
         jobstarter = js_dict[args.jobstarter]
     else:
         jobstarter = None
 
+    runner_dict = {
+        "ESMFold": {
+            "runner": ESMFold() if protslurm.config.ESMFOLD_PYTHON_PATH and protslurm.config.ESMFOLD_PYTHON_PATH else None,
+            "poses_options": {"poses": "input_files/fastas/", "glob_suffix": "*.fasta"},
+            "runner_options": {"jobstarter": jobstarter},
+            "config": [protslurm.config.ESMFOLD_PYTHON_PATH, protslurm.config.ESMFOLD_PYTHON_PATH]
+        },
+        "Rosetta": {
+            "runner": Rosetta() if protslurm.config.ROSETTA_BIN_PATH else None,
+            "poses_options": {"poses": "input_files/pdbs/", "glob_suffix": "*.pdb"},
+            "runner_options": {
+                "rosetta_application": "rosetta_scripts.linuxgccrelease",
+                "nstruct": 5,
+                "options": "-parser:protocol input_files/rosettascripts/empty.xml -beta",
+                "jobstarter": jobstarter
+            },
+            "config": [protslurm.config.ROSETTA_BIN_PATH]
+        },
+        "AttnPacker": {
+            "runner": AttnPacker() if protslurm.config.ATTNPACKER_DIR_PATH and protslurm.config.ATTNPACKER_PYTHON_PATH else None,
+            "poses_options": {"poses": "input_files/pdbs/", "glob_suffix": "*.pdb"},
+            "runner_options": {"overwrite": True, "jobstarter": jobstarter},
+            "config": [protslurm.config.ATTNPACKER_DIR_PATH, protslurm.config.ATTNPACKER_PYTHON_PATH]
+        },
+        "LigandMPNN": {
+            "runner": LigandMPNN() if protslurm.config.LIGANDMPNN_SCRIPT_PATH and protslurm.config.LIGANDMPNN_PYTHON_PATH else None,
+            "poses_options": {"poses": "input_files/pdbs/", "glob_suffix": "*.pdb"},
+            "runner_options": {
+                "model_type": "ligand_mpnn",
+                "nseq": 2,
+                "jobstarter": jobstarter
+            },
+            "config": [protslurm.config.LIGANDMPNN_SCRIPT_PATH, protslurm.config.LIGANDMPNN_PYTHON_PATH]
+        },
+        "RFdiffusion": {
+            "runner": RFdiffusion() if protslurm.config.RFDIFFUSION_SCRIPT_PATH and protslurm.config.RFDIFFUSION_PYTHON_PATH else None,
+            "poses_options": {"poses": "input_files/rfdiffusion/", "glob_suffix": "*.pdb"},
+            "runner_options": {
+                "options": "diffuser.T=50 potentials.guide_scale=5 'contigmap.contigs=[Q1-21/0 20/A1-5/10-50/B1-5/10-50/C1-5/10-50/D1-5/20]' contigmap.length=200-200 'contigmap.inpaint_seq=[A1/A2/A4/A5/B1/B2/B4/B5/C1/C2/C4/C5/D1/D2/D4/D5]' potentials.substrate=LIG",
+                "jobstarter": jobstarter
+            },
+            "config": [protslurm.config.RFDIFFUSION_SCRIPT_PATH, protslurm.config.RFDIFFUSION_PYTHON_PATH]
+        },
+        "TMscore": {
+            "runner": TMscore(),
+            "poses_options": {"poses": "input_files/rfdiffusion/", "glob_suffix": "*.pdb"},
+            "runner_options": {
+                "jobstarter": jobstarter
+            },
+            "config": [protslurm.config.RFDIFFUSION_SCRIPT_PATH, protslurm.config.RFDIFFUSION_PYTHON_PATH]
+        },
+        "TMalign": {
+            "runner": TMalign(),
+            "poses_options": {"poses": "input_files/rfdiffusion/", "glob_suffix": "*.pdb"},
+            "runner_options": {
+                "jobstarter": jobstarter
+            },
+            "config": None
+        },
+        "ProtParam": {
+            "runner": ProtParam(),
+            "poses_options": {"poses": "input_files/rfdiffusion/", "glob_suffix": "*.pdb"},
+            "runner_options": {
+                "pH": 6,
+                "jobstarter": jobstarter
+            },
+            "config": None
+        }
+    }
 
+    if protslurm.config.AUXILIARY_RUNNER_SCRIPTS_DIR == "" or not os.path.isdir(protslurm.config.AUXILIARY_RUNNER_SCRIPTS_DIR):
+        logging.warning(f"AUXILIARY_RUNNER_SCRIPTS_DIR was not properly set in config.py!")
+        runner_dict['AUXILIARY_RUNNER_SCRIPTS_DIR'] = "NOT SET UP CORRECTLY!"
+
+    if args.runner and not args.runner in runner_dict:
+        raise KeyError(f"Runner must be one of {[i for i in runner_dict]}!")
     
-####################### ESMFold #######################
-    
-    if os.path.isfile(protslurm.config.ESMFOLD_SCRIPT_PATH):
-        logging.info("Running test for ESMFold...")
-        out_dir = "output_esmfold"
-        if os.path.isdir(out_dir): shutil.rmtree(out_dir)
 
-        proteins = Poses(
-            poses="input_files/esmfold/",
-            glob_suffix="*.fasta",
-            work_dir=out_dir,
-            storage_format="json"
-        )
 
-        esm_runner = ESMFold()
-        proteins = esm_runner.run(poses=proteins, prefix="test", overwrite=True, jobstarter=jobstarter)
+    for runner in runner_dict:
+        runner_dict[runner] = check_runner(name=runner, runner=runner_dict[runner]["runner"], poses_options=runner_dict[runner]["poses_options"], runner_options=runner_dict[runner]["runner_options"], config=runner_dict[runner]["config"], check=args.runner)
 
-        logging.info("ESMFold passed!")
+    print(runner_dict)
+
+'''
+
+
 
 ####################### Sequence Logo #######################
     
+    if not args.runner or args.runner == "SequenceLogo":
+        logging.info("Running test for sequence logo generation...")
+        try:
+            out_dir = "output_seqlogo"
+            if os.path.isdir(out_dir): shutil.rmtree(out_dir)
 
-    logging.info("Running test for sequence logo generation...")
-    out_dir = "output_seqlogo"
-    if os.path.isdir(out_dir): shutil.rmtree(out_dir)
+            proteins = Poses(
+                poses="input_files/esmfold/",
+                glob_suffix="*.fasta",
+                work_dir=out_dir,
+                storage_format="json"
+            )
 
-    proteins = Poses(
-        poses="input_files/esmfold/",
-        glob_suffix="*.fasta",
-        work_dir=out_dir,
-        storage_format="json"
-    )
+            sequence_logo(dataframe=proteins.df, input_col="poses", save_path=os.path.join(out_dir, "seq.logo"), refseq="AS", title=None, resnums=[1,2], units="probability")
 
-    sequence_logo(dataframe=proteins.df, input_col="poses", save_path=os.path.join(out_dir, "seq.logo"), refseq="AS", title=None, resnums=[1,2], units="probability")
+            logging.info("ESMFold passed!")
+            runner_dict['SequenceLogo'] = "Passed"
+        except:
+            runner_dict['SequenceLogo'] = "Failed"
 
-    logging.info("ESMFold passed!")
-
-####################### AttnPacker #######################
-
-    if os.path.isdir(protslurm.config.ATTNPACKER_DIR_PATH):
-        logging.info("Running test for AttnPacker...")
-        out_dir = "output_attnpacker"
-        if os.path.isdir(out_dir): shutil.rmtree(out_dir)
-
-        proteins = Poses(
-            poses="input_files/attnpacker/",
-            glob_suffix="*.pdb",
-            work_dir=out_dir,
-            storage_format="json"
-        )
-
-        proteins = AttnPacker().run(poses=proteins, prefix="test", overwrite=False, jobstarter=jobstarter)
-
-        logging.info("AttnPacker passed!")
 
 ####################### TMalign #######################
 
@@ -117,8 +195,8 @@ def main(args):
         storage_format="csv",
         jobstarter=LocalJobStarter()
     )
-
-    proteins = TMalign().run(poses=proteins, prefix="test", ref_col="poses", overwrite=True, jobstarter=jobstarter)
+    proteins.df['ref_col'] = proteins.df['poses']
+    proteins = TMalign().run(poses=proteins, prefix="test", ref_col="ref_col", overwrite=True, jobstarter=jobstarter)
 
 
     logging.info("TMalign passed!")
@@ -144,91 +222,7 @@ def main(args):
     logging.info("TMalign passed!")
 
 
-####################### ProtParam #######################
-
-    logging.info("Running test for ProtParam...")
-    out_dir = "output_protparam"
-    if os.path.isdir(out_dir): shutil.rmtree(out_dir)
-
-    proteins = Poses(
-        poses="input_files/rosettascripts/",
-        glob_suffix="*.pdb",
-        work_dir=out_dir,
-        storage_format="csv",
-        jobstarter=LocalJobStarter()
-    )
-
-    proteins = ProtParam().run(poses=proteins, prefix="test", pH=6, overwrite=True, jobstarter=jobstarter)
-
-
-    logging.info("ProtParam passed!")
-
-####################### ROSETTA #######################
-
-    if os.path.isdir(protslurm.config.ROSETTA_BIN_PATH):
-        logging.info("Running test for Rosetta...")
-        out_dir = "output_rosetta"
-        #if os.path.isdir(out_dir): shutil.rmtree(out_dir)
-
-        proteins = Poses(
-            poses="input_files/rosettascripts/",
-            glob_suffix="*.pdb",
-            work_dir=out_dir,
-            storage_format="json"
-        )
-
-        options = "-parser:protocol input_files/rosettascripts/empty.xml -beta"
-        proteins = Rosetta().run(poses=proteins, prefix="test", rosetta_application="rosetta_scripts.linuxgccrelease", nstruct=2, options=options, overwrite=True, jobstarter=jobstarter)
-        proteins.calculate_composite_score(name="comp_score", scoreterms=["test_total_score", "test_fa_dun_rot", "test_fa_elec"], weights=[-1, 1, -1], plot=True)
-        proteins.filter_poses_by_rank(n=1, score_col="test_total_score", remove_layers=1, prefix="test_rank", plot=True)
-        proteins.filter_poses_by_value(value=3000, score_col="test_total_score", operator="<=", prefix="test_value", plot=True)
-
-        logging.info("Rosetta passed!")
-
-####################### LIGANDMPNN #######################
-
-    if os.path.isfile(protslurm.config.LIGANDMPNN_SCRIPT_PATH):
-        logging.info("Running test for LigandMPNN...")
-        out_dir = "output_ligandmpnn"
-        if os.path.isdir(out_dir): shutil.rmtree(out_dir)
-
-        proteins = Poses(
-            poses="input_files/ligandmpnn/",
-            glob_suffix="*.pdb",
-            work_dir=out_dir,
-            storage_format="feather"
-        )
-
-        #set fixed residues
-        proteins.df['fixed_residues'] = ['A3,B3,C3,D3']
-        # start ligand_mpnn
-        proteins = LigandMPNN().run(poses=proteins, prefix="test", model_type="ligand_mpnn", nseq=5, fixed_res_col='fixed_residues', jobstarter=jobstarter)
-
-        logging.info("LigandMPNN passed!")
-
-
-####################### RFDIFFUSION #######################
-
-
-    if os.path.isfile(protslurm.config.RFDIFFUSION_SCRIPT_PATH):
-        logging.info("Running test for RFdiffusion...")
-        out_dir = "output_rfdiffusion"
-        if os.path.isdir(out_dir): shutil.rmtree(out_dir)
-
-        proteins = Poses(
-            poses="input_files/rfdiffusion/",
-            glob_suffix="*.pdb",
-            work_dir=out_dir,
-            storage_format="json"
-        )
-
-        options = "diffuser.T=50 potentials.guide_scale=5 'contigmap.contigs=[Q1-21/0 20/A1-5/10-50/B1-5/10-50/C1-5/10-50/D1-5/20]' contigmap.length=200-200 'contigmap.inpaint_seq=[A1/A2/A4/A5/B1/B2/B4/B5/C1/C2/C4/C5/D1/D2/D4/D5]' potentials.substrate=LIG"
-        proteins = RFdiffusion().run(poses=proteins, prefix="test", num_diffusions=1, options=options, overwrite=True, jobstarter=jobstarter)
-
-        logging.info("RFdiffusion passed!")
-
-
-
+'''
 
 if __name__ == "__main__":
 
@@ -238,6 +232,7 @@ if __name__ == "__main__":
     import argparse
     argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     argparser.add_argument("--jobstarter", type=str, default=None, help="define jobstarter. can be either slurm_gpu_jobstarter or local_jobstarter, if None default slurm is used.")
+    argparser.add_argument("--runner", type=str, default=None, help="Only test runner named <runner>. See runner_dict for available runners.")
     args = argparser.parse_args()
     #run main
     main(args)
