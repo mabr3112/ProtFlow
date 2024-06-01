a35c4e99406a1492b53b2e07436b239106347116
Markus Braun <markus.braun95@gmail.com>
2024-06-01 22:49:11 +0200

    updated source

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))  # Adjust the path to your module's location
+sys.path.insert(0, os.path.abspath('../../protflow/'))  # Adjust the path to your module's location
 
 # -- Project information -----------------------------------------------------
 
