0d779580820bd1cb387d3e88d8c32c542851c18d
TecnomaLaser <adrian.tripp1@gmail.com>
2024-06-20 09:21:06 +0200

    removed logs

--- protflow/metrics/tmscore.py
+++ protflow/metrics/tmscore.py
@@ -303,10 +303,7 @@ class TMalign(Runner):
         )
 
         scorefile = os.path.join(work_dir, f"{prefix}_TM.{poses.storage_format}")
-        logging.info(work_dir)
-        logging.info(prefix)
-        logging.info(poses.storage_format)
-        logging.info(scorefile)
+        
         if (scores := self.check_for_existing_scorefile(scorefile=scorefile, overwrite=overwrite)) is not None:
             output = RunnerOutput(poses=poses, results=scores, prefix=prefix).return_poses()
             if sc_tm_score:
