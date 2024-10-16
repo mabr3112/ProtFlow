5a405fc7bdc2b94f7bc18cf67f9a79ccd89135e5
TecnomaLaser <adrian.tripp1@gmail.com>
2024-10-16 13:27:10 +0200

    added examples

--- protflow/config_template.py
+++ protflow/config_template.py
@@ -2,7 +2,7 @@
 This module contains all paths to tools integrated in ProtFlow.
 '''
 # protflow environment python
-PROTFLOW_ENV = ""
+PROTFLOW_ENV = "" # "/path/to/mambaforge/envs/protflow/bin/"
 
 # auxiliary runners directory path
 #TODO: @Markus Implement checkup if AUXILIARY_RUNNER_SCRIPTS_DIR points to the correct directory (Think about pointing to protflow directory)!
@@ -12,28 +12,28 @@ AUXILIARY_RUNNER_SCRIPTS_DIR = ""
 PROTEIN_GENERATOR_SCRIPT_PATH = ""
 
 # ligandmpnn.py
-LIGANDMPNN_SCRIPT_PATH = ""
-LIGANDMPNN_PYTHON_PATH = ""
-
-# rosettascripts.py
-ROSETTA_SCRIPTS_PATH = ""
+LIGANDMPNN_SCRIPT_PATH = "" # "/path/to/LigandMPNN/run.py"
+LIGANDMPNN_PYTHON_PATH = "" # "/path/to/anaconda3/envs/ligandmpnn_env/bin/python3"
 
 # rosetta.py
-ROSETTA_BIN_PATH = ""
+ROSETTA_BIN_PATH = "" # "/path/to/Rosetta/main/source/bin/"
 
 # attnpacker.py
-ATTNPACKER_DIR_PATH = ""
-ATTNPACKER_PYTHON_PATH = ""
+ATTNPACKER_PYTHON_PATH = "" # "/path/to/anaconda3/envs/attnpacker/bin/python3"
+ATTNPACKER_DIR_PATH = "" # "/path/to/AttnPacker/"
 
 # rfdiffusion.py
-RFDIFFUSION_SCRIPT_PATH = ""
-RFDIFFUSION_PYTHON_PATH = ""
+RFDIFFUSION_SCRIPT_PATH = "" # "/path/to/RFdiffusion/scripts/run_inference.py"
+RFDIFFUSION_PYTHON_PATH = "" # "/path/to/miniconda3/envs/SE3nv/bin/python"
 
 # esmfold.py
-ESMFOLD_PYTHON_PATH = ""
+ESMFOLD_PYTHON_PATH = "" # "/path/to/miniconda3/envs/esm/bin/python"
 
 # af2
-COLABFOLD_SCRIPT_PATH = ""
+COLABFOLD_SCRIPT_PATH = "" # "/path/to/localcolabfold/colabfold-conda/bin/colabfold_batch"
 
 # fpocket
-FPOCKET_PATH = ""
+FPOCKET_PATH = "" # "/path/to/anaconda3/envs/protslurm/bin/fpocket"
+
+# dssp
+DSSP_PATH = "" # "/path/to/mambaforge/envs/dssp/bin/mkdssp"
