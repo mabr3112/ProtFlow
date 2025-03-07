83d1109f97265d030d81f5a796beda542a20e9d5
mabr3112 <markus.braun95@gmail.com>
2025-03-08 00:46:57 +0100

    implemented scripts to download RCSB advanced queries in batch download mode

---
+++ scripts/batch_download_rcsb_query.py
@@ -0,0 +1,151 @@
+'''Script that downloads all hits for an advanced RCSB PDB query in batches. Usage see below.'''
+# imports
+import os
+import sys
+import json
+import logging
+import requests
+
+# dependencies
+import pandas as pd
+
+# custom
+import protflow
+
+def main(args):
+    '''does stuff'''
+    os.makedirs(args.output_dir, exist_ok=True)
+    # setup logging
+    logging.basicConfig(
+        level=logging.INFO,  # Change to DEBUG for more details
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[
+            logging.FileHandler(os.path.join(args.output_dir, "log.txt")),  # Log to file
+            logging.StreamHandler()  # Log to console
+        ]
+    )
+    logging.info(f"Running batch_download_rcsb_query.py on {args.query_file}. Downloading files to {args.output_dir}")
+
+    # read query from file:
+    if not os.path.isfile(args.query_file):
+        raise FileNotFoundError(args.query_file)
+
+    ############ collect query hits ###################
+    logging.info(f"Loading RCSB advanced query from file {args.query_file}")
+    with open(args.query_file, 'r', encoding="UTF-8") as f:
+        query_dict = json.load(f)
+
+    # RCSB Search API endpoint
+    url = "https://search.rcsb.org/rcsbsearch/v2/query"
+
+    # Send the POST request with the JSON payload
+    try:
+        response = requests.post(url, json=query_dict, timeout=900)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        raise RuntimeError(f"RCSB query failed: {e}") from e
+
+    # process result
+    if response.status_code == 200: # successful request
+        results = response.json()
+        # Save the results to a file on your computer
+        with open(os.path.join(args.output_dir, "rcsb_results.json"), "w", encoding="UTF-8") as f:
+            json.dump(results, f, indent=2)
+        logging.info(f"Successfully retrieved {len(results.get('result_set', []))} results from RCSB query.")
+    else: # request has hit an error
+        raise RuntimeError(response.status_code, response.text)
+
+    # if no hits, finish script
+    if not results:
+        logging.info(f"Query yielded no hits. Query file: {args.query_file}")
+        sys.exit(0)
+
+    ############## download pdbs into organized folder structure ################
+    # count number of hits and prepare download
+    pdb_ids = [entry["identifier"] for entry in results.get("result_set", [])]
+    logging.info(f"Found {len(pdb_ids)} entries.")
+
+    # split pdb_ids into batches
+    pdb_batches = protflow.jobstarters.split_list(pdb_ids, args.batch_size)
+
+    # create output directory
+    batch_input_dir = os.path.join(args.output_dir, "batch_input_files")
+    os.makedirs(batch_input_dir, exist_ok=True)
+
+    # store index of which file is where in .csv format for accessibility (relative and absolute)
+    cmds = []
+    index_dict = {}
+    executable = f"python {protflow.config.PROTFLOW_DIR}/scripts/helper_scripts/download_pdbs.py"
+    for i, pdb_batch in enumerate(pdb_batches):
+        # write pdb_batches to file
+        batch_file = os.path.join(batch_input_dir, f"pdb_ids_{i+1}.csv")
+        with open(batch_file, 'w', encoding="UTF-8") as f:
+            f.write(",".join(pdb_batch))
+
+        # setup batch output dir:
+        batch_fn = f"downloads/batch_{str(i+1).zfill(4)}"
+        batch_download_dir = os.path.join(args.output_dir, batch_fn)
+
+        # aggregate download command to cmd-list
+        cmds.append(f'{executable} --input_path "{batch_file}" --output_dir "{batch_download_dir}" --format {args.format}')
+
+        # add batch_filename -> pdb_ids mapping into index
+        index_dict[batch_fn] = pdb_batch
+
+    # setup jobstarter
+    if args.jobstarter == "sbatch":
+        jst = protflow.jobstarters.SbatchArrayJobstarter(max_cores=args.num_workers)
+    elif args.jobstarter == "local":
+        jst = protflow.jobstarters.LocalJobStarter(max_cores=args.num_workers)
+    else:
+        raise ValueError(f"Unsupported options for --jobstarter: {args.jobstarter}. Allowed options: {{sbatch, local}}")
+
+    # run cmds
+    jst.start(
+        cmds=cmds,
+        jobname="rcsb_query_download",
+        wait=True,
+        output_path=args.output_dir
+    )
+
+    ############### store pdb locations in .csv file for later easier retrieval #################
+    # aggregate locations into DataFrame
+    inverted_index = {pdb_id: batch_dir for batch_dir, pdb_ids in index_dict.items() for pdb_id in pdb_ids} # standard dict inversion
+    index_dict_full = {
+        "pdb_id": list(inverted_index.keys()),
+        "relative_path": list(inverted_index.values()),
+        "absolute_path": [os.path.join(args.output_dir, relpath) for relpath in list(inverted_index.values())]
+    }
+
+    # store DataFrame in output_dir
+    index_fn = os.path.join(args.output_dir, "download_index.df.csv")
+    logging.info(f"Storing index of which pdb-file is where at {index_fn}")
+    index_df = pd.DataFrame(index_dict_full)
+    index_df = index_df.set_index("pdb_id", drop=True)
+    index_df.to_csv(index_fn)
+
+    # finish script
+    logging.info("Done.")
+
+if __name__ == "__main__":
+    import argparse
+
+    # setup args
+    argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    argparser.add_argument("--query_file", type=str, required=True, help="Path to the file that contains the query (copied from RCSB advanced query page)")
+    argparser.add_argument("--output_dir", type=str, required=True, help="Path to root directory of where the files should be stored.")
+    argparser.add_argument("--batch_size", type=int, default=1000, help="Size of the batches in which pdb files should be grouped.")
+    argparser.add_argument("--format", type=str, default="cif", help="{cif, pdb, fa} File format of downloads.")
+    argparser.add_argument("--jobstarter", type=str, default="sbatch", help="{sbatch, local} Specify which jobstarter class to use for batch downloads.")
+    argparser.add_argument("--num_workers", type=int, default=32, help="Number of parallel processes to start for download.")
+    arguments = argparser.parse_args()
+
+    # check arguments (either input_json or input_pdb + reference_pdb)
+    if arguments.format not in {"cif", "pdb", "fa"}:
+        raise ValueError(f"Unsupported download format specified by --format: {arguments.format}. Allowed formats: {{cif, pdb, fa}}")
+    if arguments.jobstarter not in {"sbatch", "local"}:
+        raise ValueError(f"Unsupported options for --jobstarter: {arguments.jobstarter}. Allowed options: {{sbatch, local}}")
+
+    # run
+    main(arguments)
---
+++ scripts/helper_scripts/download_pdbs.py
@@ -0,0 +1,86 @@
+'''Script to download pdbs from RCSB when given file with list of pdb_ids.'''
+# imports
+import os
+import json
+import logging
+import requests
+
+def main(args):
+    '''do stuff'''
+    os.makedirs(args.output_dir)
+    # setup logging
+    logging.basicConfig(
+        level=logging.INFO,  # Change to DEBUG for more details
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[
+            logging.FileHandler(os.path.join(args.output_dir, "log.txt")),  # Log to file
+            logging.StreamHandler()  # Log to console
+        ]
+    )
+    logging.info(f"Running download_pdbs.py on {args.input_path}")
+
+    # load list of pdb ids
+    with open(args.input_path, 'r', encoding="UTF-8") as f:
+        if args.input_path.endswith(".json"):
+            pdb_list = json.load(f)
+
+        elif args.input_path.endswith(".csv"):
+            pdb_list = [pdb_id.strip() for pdb_id in f.read().split(",")]
+
+        else:
+            raise ValueError(f"File --input_path must be .json or .csv file and should contain list of pdb_ids. --input_path: {args.input_path}")
+
+    # check correct format and files
+    if not pdb_list:
+        raise ValueError(f"No pdb_ids specified in input file: {args.input_path}")
+    if not isinstance(pdb_list, list):
+        raise ValueError(f"pdb_list not loaded as list. Check formatting of input file: {args.input_path}")
+
+    # Download each PDB file
+    failed_downloads = []
+    logging.info(f"Starting to download {len(pdb_list)} files from RCSB in {args.format} format.")
+    for pdb_id in pdb_list:
+        # define file to download
+        file_path = os.path.join(args.output_dir, f"{pdb_id}.{args.format}")
+
+        # skip download, if file is already there:
+        if os.path.isfile(file_path):
+            continue
+
+        pdb_url = f"https://files.rcsb.org/download/{pdb_id}.{args.format}"
+        try:
+            pdb_response = requests.get(pdb_url, timeout=90)
+            pdb_response.raise_for_status()
+        except requests.RequestException as e:
+            logging.error(f"Error downloading {pdb_id}.{args.format}: {e}")
+            failed_downloads.append(pdb_id)
+            continue
+
+        # write pdb_id.format file
+        with open(file_path, "w", encoding="UTF-8") as f:
+            f.write(pdb_response.text)
+
+    # log failed downloads
+    if failed_downloads:
+        with open(os.path.join(args.output_dir, "failed_downloads.csv"), 'w', encoding="UTF-8") as f:
+            f.write(",".join(failed_downloads))
+
+    # log
+    logging.info(f"Downloaded {len(pdb_list) - len(failed_downloads)} of {len(pdb_list)} pdb files from RCSB in {args.format} format.")
+    logging.info("Done.")
+
+if __name__ == "__main__":
+    import argparse
+
+    # setup args
+    argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    argparser.add_argument("--input_path", type=str, required=True, help="Path to file that holds [json serialized / or csv] list of pdb_ids.")
+    argparser.add_argument("--output_dir", type=str, required=True, help="Path to directory where pdb files should be stored.")
+    argparser.add_argument("--format", type=str, default="cif", help="{cif, pdb, fa} File format of downloads.")
+    arguments = argparser.parse_args()
+
+    if arguments.format not in {"cif", "pdb", "fa"}:
+        raise ValueError(f"Unsupported download format specified by --format: {arguments.format}. Allowed formats: {{cif, pdb, fa}}")
+
+    main(arguments)
