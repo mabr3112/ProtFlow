81198c9156a818537021fd9c5c8532aeb0d00a28
TecnomaLaser <adrian.tripp1@gmail.com>
2026-05-11 10:58:24 +0200

    missing overwrite pass to query

--- protflow/metrics/hbplus.py
+++ protflow/metrics/hbplus.py
@@ -759,7 +759,7 @@ class HBplus(Runner):
         poses = RunnerOutput(poses, scores, prefix, index_layers=self.index_layers).return_poses()
 
         if queries:
-            poses = self.query(poses=poses, queries=queries, hbplus_prefix=prefix)
+            poses = self.query(poses=poses, queries=queries, hbplus_prefix=prefix, overwrite=overwrite)
         return poses
     
     def query(self, poses: Poses, queries: list[HBplus_query] | HBplus_query, hbplus_prefix: str, jobstarter: JobStarter = None, full_output: bool = False, overwrite: bool = False):
